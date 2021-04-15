a2ea967fbf8da91e663a47ab6c56a804c2a07811
blen_meku <mekuriablen@gmail.com>
2021-04-14 22:44:31 -0700

    modified layouts and views

---
+++ app/javascript/packs/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import App from "../src/components/App";
+import PropTypes from "prop-types";
+const Hello = (props) => <div>Hello{props.name}!</div>;
+
+Hello.defaultProps = {
+  name: "David",
+};
+
+Hello.propTypes = {
+  name: PropTypes.string,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    // <Router>
+    //   <Route path="/" component={App} />
+    // </Router>,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-namespace :api do
+  namespace :api do
     namespace :v1 do 
       resources :diets
       resources :reviews, only: [:create, :destroy]
     end
   end
-
+#  get '*path', to: 'pages#index', via: :all
 end
