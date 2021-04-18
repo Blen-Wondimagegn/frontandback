a0f38dfd1dd78d4d621f0879005646f0b168b17e
blen_meku <mekuriablen@gmail.com>
2021-04-17 21:47:21 -0700

    added react router

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import App from "../src/components/App";
-import PropTypes from "prop-types";
-
-const Hello = (props) => <div>Hello {props.name}!</div>;
-
-Hello.defaultProps = {
-  name: "David",
-};
-
-Hello.propTypes = {
-  name: PropTypes.string,
-};
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import App from "../src/components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    // <Router>
-    //   <Route path="/" component={App} />
-    // </Router>,
+    <Router>
+      <Route path="/" component={App} />
+    </Router>,
     document.body.appendChild(document.createElement("div"))
   );
 });
